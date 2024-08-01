133aac4c74cc1faca4667d9bbe2bcf470d6a239c
Dennis <deynohmuturi@gmail.com>
2024-08-01 13:55:04 +0300

    feat: Add updateOrderPaymentDetails and changePaymentCompletionStatus functions

--- firebaseUtils/firebaseConfig.js
+++ firebaseUtils/firebaseConfig.js
@@ -8,7 +8,6 @@ async function fetchOrderItemsDetails(orderId){
     const ordersRef=db.collection('orders').doc(orderId);
     const ordersResponse=await ordersRef.get()
     const finalDetails=await Promise.all(ordersResponse.data().orderItems.map(async(item)=>{
-        console.log(item.productId)
         const productsRef = db.collection("products").where('pid','==',item.productId);
         const productResponse = await productsRef.get();
         const {title,price}=productResponse.docs[0].data()
@@ -16,7 +15,27 @@ async function fetchOrderItemsDetails(orderId){
     }))
     return finalDetails
 }
+  async function updateOrderPaymentDetails(orderId,sessionId){
+    const ordersRef=db.collection('orders').doc(orderId)
+    await ordersRef.update({typeOfPayment:'stripe',sessionId})
+  }
+  async function changePaymentCompletionStatus(sessionId){
+    console.log(sessionId)
+     const ordersRef = db.collection("orders").where('sessionId','==',sessionId)
+     const orders=await ordersRef.get()
+     console.log('orderId',orders.docs[0].id)
+     const orderRef = db.collection("orders").doc(orders.docs[0].id)
+     await orderRef.update({paid:true,shipped:false});
+     const cartItemsRef = db
+       .collection("cartItems")
+       .where("userId", "==", orders.docs[0].data().userId);
+       const cartItems=await cartItemsRef.get()
+       cartItems.docs.forEach(async (item)=>{
+        const itemRef=db.collection('cartItems').doc(item.id)
+        await itemRef.delete()
+       })
+  }
 function getTotalPrice(orderItems){
     return orderItems.reduce((acc,curr)=>acc+curr.quantity*curr.price,0)
 }
-module.exports={fetchOrderItemsDetails,getTotalPrice}
+module.exports={fetchOrderItemsDetails,getTotalPrice,updateOrderPaymentDetails,changePaymentCompletionStatus}
